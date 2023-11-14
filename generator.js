17755a60b0b7139d7cb6812048d92ea0c92e1c91
Severin Bruhat <sbruhat@gmail.com>
2023-11-14 17:32:38 +0000

    fix the patterns

--- generator.js
+++ generator.js
@@ -14,35 +14,55 @@ var pitches = [
 ]
 
 const Scales = {
-    WHOLETONE: [0, 2, 4, 6, 8, 10, 12], //whole step
-    MAJORTRIAD: [0, 4, 7], //root, major third and perfect fifth.
-    MINORTRIAD: [0, 3, 7], //root, a minor third and a perfect fifth
+    WHOLETONE: [2, 2, 2, 2, 2, 2], //whole step
+    MAJORTRIAD: [0, 4, 3], //root, major third and perfect fifth. [0, 4, 7]
+    MINORTRIAD: [0, 3, 4], //root, a minor third and a perfect fifth [0, 3, 7]
+    MAJOR: [2, 2, 1, 2, 2, 2, 1],
+    MINOR: [2, 1, 2, 2, 1, 2, 2]
 }
-
-function generateScale(rootNote, pattern){
+function generateScale(rootNote, pattern) {
     const rootIndex = pitches.indexOf(rootNote);
     if (rootIndex === -1) {
       return "Invalid root note";
     }
   
-    const scale = [];
-
-    for (let i = 0; i < pattern.length; i++) {
-        scale.push(pitches[(rootIndex + pattern[i]) % 12]);
-    }
+    const scale = [rootNote];
+    let currentIndex = rootIndex;
   
+    for (let i = 0; i < pattern.length; i++) {
+      currentIndex = (currentIndex + pattern[i]) % 12;
+      scale.push(pitches[currentIndex]);
+    }
+
+    //hack for triads as we only want 3 notes
+    if(pattern.toString() == Scales.MAJORTRIAD.toString() || pattern.toString() == Scales.MINORTRIAD.toString())
+        return scale.slice(1);
+        
     return scale;
-}
+  }
 
 
 // Example usage:
-const cWholeToneScale = generateScale("C", Scales.WHOLETONE);
-console.log("C Whole-Tone Scale:", cWholeToneScale);
-  
+// const cWholeToneScale = generateScale("C", Scales.WHOLETONE);
+// console.log("C Whole-Tone Scale:", cWholeToneScale);
 
-const fMajorTriad = generateScale("F", Scales.MAJORTRIAD);
-console.log("F Major Triad:", fMajorTriad);
+// const fMajorTriad = generateScale("F", Scales.MAJORTRIAD);
+// console.log("F Major Triad:", fMajorTriad);
 
+// const eMinorTriad = generateScale("E", Scales.MINORTRIAD);
+// console.log("E Minor Triad:", eMinorTriad);
+
+// const cMajorScale = generateScale("C", Scales.MAJOR);
+// console.log("C Major Scale:", cMajorScale);
+
+// const aNaturalMinorScale = generateScale("A", Scales.MINOR);
+// console.log("A Natural Minor Scale:", aNaturalMinorScale);
+
+const cMajorTriad = generateScale("F", Scales.MAJORTRIAD);
+console.log("C Major Triad:", cMajorTriad);
+
+const fWholeTone = generateScale("F", Scales.WHOLETONE);
+console.log("F Whole Tone:", fWholeTone);
 
 //TODO
 // scales + chords
